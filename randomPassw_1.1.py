740bd6d15d8634e7e52ba5d21a679319f44b9743
Jarantym <32854148+Jarantym@users.noreply.github.com>
2018-01-08 15:13:17 +0300

    Update randomPassw_1.1.py

--- randomPassw_1.1.py
+++ randomPassw_1.1.py
@@ -3,14 +3,14 @@
 from random import randint
 
 def main() :
-    for j in range(10):
+    for j in range(10): ## strings and characters mixing
         for i in range(4) :
             a= str(randomCharacter("bcdfghjk1mnpqrstvwxz"))
             b= str(randomCharacter("aei0uy"))
             print(a+b,end='')
         print()
         
-    
+## random function chosing inputs    
 def randomCharacter(characters) :
     n = len(characters)
     r = randint(0, n - 1)
